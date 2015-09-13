3bcfc0524e3d0c05b57b6a7f8a4f1735a855d4f3
Martijn Hoekstra <martijnhoekstra@gmail.com>
2015-09-13 22:48:53 +0200

    license headers

--- src/main/scala/Handling.scala
+++ src/main/scala/Handling.scala
@@ -1,3 +1,20 @@
+/*
+  This file is part of Rimbot.
+
+  Rimbot is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Foobar is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package net.fgsquad.rimbot
 
 import scalaz.concurrent.Task
@@ -6,7 +23,6 @@ import org.jibble.pircbot._
 object Handling {
   def asMod[T](bot: PircBot, channel: String, getmods: Task[List[String]]): (List[String], String) => Task[T] => Task[(List[String], Option[T])] =
     (mods, user) => (taction) =>
-      //(mods: List[String], user: String) => (action: =>T) => {
       if (mods.contains(user)) taction.map(t => (mods, Some(t)))
       else getmods.flatMap(newmods => {
         bot.log(s"new mods are $newmods")
